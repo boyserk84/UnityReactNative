57c83f22adc310c666ef2a603409bf84c5eecd06
Nate K <nkemavaha@zynga.com>
2018-07-14 11:14:07 -0700

    Add gameobjets to mainscene.unity and adjust button position.

--- UnityClient/Assets/Scripts/ReactManager.cs
+++ UnityClient/Assets/Scripts/ReactManager.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class ReactManager : MonoBehaviour {
 
 
-    void OnGUI() {
-        if (GUI.Button(new Rect(10, 70, 200, 30), "Launch react native activity")) {
+    void OnGUI() 
+    {
+        if (GUI.Button(new Rect(Screen.width/2 - 100 , Screen.height/2 - 50, 250, 200), "Launch react native activity")) {
             Debug.Log("Button clicked");
 
             AndroidJavaClass unityPlayerClass = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
             AndroidJavaObject currentActivity = unityPlayerClass.GetStatic<AndroidJavaObject>("currentActivity");
 
-            AndroidJavaClass reactNativeActivity = new AndroidJavaClass("com.nkemavaha.react.ReactNativeActivity");
-            reactNativeActivity.CallStatic("Launch", currentActivity);
+            AndroidJavaClass reactNativeActivity = new AndroidJavaClass("com.nkemavaha.natereactnativeandroidmodule.ReactLauncher");
+            reactNativeActivity.CallStatic("StartReact", currentActivity);
         }
     }
 
