f0cb257d76e723dfb9b7ad1906b10bc5551892d6
Nate K <nkemavaha@zynga.com>
2018-07-13 23:00:41 -0700

    Add Android Native Project and Module.

---
+++ ReactAndroid/ReactProject/native_android/natereactnativeandroidmodule/src/main/java/com/nkemavaha/natereactnativeandroidmodule/ReactAndroidActivity.java
@@ -0,0 +1,96 @@
+package com.nkemavaha.natereactnativeandroidmodule;
+
+import android.app.Activity;
+import android.arch.core.BuildConfig;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+
+public class ReactAndroidActivity extends AppCompatActivity implements DefaultHardwareBackBtnHandler{
+
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    private final int OVERLAY_PERMISSION_REQ_CODE = 1;  // Choose any value
+
+    private final String REACTNATIVE_MODULE_NAME = "ReactProject";
+
+    /**
+     * Start React Activity from an external call.
+     * @param activity
+     */
+    public static void StartReact(Activity activity) {
+        activity.startActivity(new Intent(activity, ReactAndroidActivity.class));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReactRootView = new ReactRootView(this);
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModulePath("index")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        // The string here (e.g. "MyReactNativeApp") has to match
+        // the string in AppRegistry.registerComponent() in index.js
+        mReactRootView.startReactApplication(mReactInstanceManager, REACTNATIVE_MODULE_NAME, null);
+
+        setContentView(mReactRootView);
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostPause(this);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostResume(this, this);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onHostDestroy(this);
+        }
+        if (mReactRootView != null) {
+            mReactRootView.unmountReactApplication();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onBackPressed();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
---
+++ ReactAndroid/ReactProject/native_android/natereactnativeandroidmodule/src/main/java/com/nkemavaha/natereactnativeandroidmodule/ReactLauncher.java
@@ -0,0 +1,10 @@
+package com.nkemavaha.natereactnativeandroidmodule;
+
+import android.app.Activity;
+
+public class ReactLauncher {
+
+    public static void StartReact(Activity acitivity) {
+        ReactAndroidActivity.StartReact(acitivity);
+    }
+}
